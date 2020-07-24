bb76b2eb580d1e59eaf14214b74f51290eb98675
Moomyd <1345449751@qq.com>
2020-07-24 23:45:39 +0800

    array

---
+++ Array/33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,34 @@
+/*
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+题解：二分搜索法的关键在于获得了中间数后，判断下面要搜索左半段还是右半段
+如果中间的数小于最右边的数，则右半段是有序的，
+若中间数大于最右边数，则左半段是有序的，
+我们只要在有序的半段里用首尾两个数组来判断目标值是否在这一区域内，
+这样就可以确定保留哪半边了
+*/
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < nums[right]) {
+                if (nums[mid] < target && nums[right] >= target) left = mid + 1;
+                else right = mid - 1;
+            } else {
+                if (nums[left] <= target && nums[mid] > target) right = mid - 1;
+                else left = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
